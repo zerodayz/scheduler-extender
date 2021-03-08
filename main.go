0f1820055947f10fd3381d2052b34807a5089865
Robin Cernin <cerninr@gmail.com>
2021-03-09 09:50:47 +1000

    .

--- main.go
+++ main.go
@@ -1,9 +1,161 @@
 package main
 
 import (
-	"github.com/zerodayz/scheduler-extender/router"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"strings"
+
+	"github.com/julienschmidt/httprouter"
+	v1 "k8s.io/api/core/v1"
+	schedulerapi "k8s.io/kube-scheduler/extender/v1"
 )
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Filter(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var buf bytes.Buffer
+	body := io.TeeReader(r.Body, &buf)
+	var extenderArgs schedulerapi.ExtenderArgs
+	var extenderFilterResult *schedulerapi.ExtenderFilterResult
+	if err := json.NewDecoder(body).Decode(&extenderArgs); err != nil {
+		extenderFilterResult = &schedulerapi.ExtenderFilterResult{
+			Error: err.Error(),
+		}
+	} else {
+		extenderFilterResult = filter(extenderArgs)
+	}
+
+	if response, err := json.Marshal(extenderFilterResult); err != nil {
+		log.Fatalln(err)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
+	}
+}
+
+func Prioritize(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var buf bytes.Buffer
+	body := io.TeeReader(r.Body, &buf)
+	var extenderArgs schedulerapi.ExtenderArgs
+	var hostPriorityList *schedulerapi.HostPriorityList
+	if err := json.NewDecoder(body).Decode(&extenderArgs); err != nil {
+		log.Println(err)
+		hostPriorityList = &schedulerapi.HostPriorityList{}
+	} else {
+		hostPriorityList = prioritize(extenderArgs)
+	}
+
+	if response, err := json.Marshal(hostPriorityList); err != nil {
+		log.Fatalln(err)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(response)
+	}
+}
+
+const (
+	// LuckyPred rejects a node if you're not lucky ¯\_(ツ)_/¯
+	LuckyPred        = "Lucky"
+	LuckyPredFailMsg = "Well, you're not lucky"
+	// lucky priority gives a random [0, extender.MaxPriority] score
+	// currently extender.MaxPriority is 10
+	luckyPrioMsg = "pod %v/%v is lucky to get score %v\n"
+)
+
+var predicatesSorted = []string{LuckyPred}
+
+var predicatesFuncs = map[string]FitPredicate{
+	LuckyPred: LuckyPredicate,
+}
+
+type FitPredicate func(pod *v1.Pod, node v1.Node) (bool, []string, error)
+
+func podFitsOnNode(pod *v1.Pod, node v1.Node) (bool, []string, error) {
+	fits := true
+	var failReasons []string
+	for _, predicateKey := range predicatesSorted {
+		fit, failures, err := predicatesFuncs[predicateKey](pod, node)
+		if err != nil {
+			return false, nil, err
+		}
+		fits = fits && fit
+		failReasons = append(failReasons, failures...)
+	}
+	return fits, failReasons, nil
+}
+
+func LuckyPredicate(pod *v1.Pod, node v1.Node) (bool, []string, error) {
+	lucky := rand.Intn(2) == 0
+	if lucky {
+		log.Printf("pod %v/%v is lucky to fit on node %v\n", pod.Name, pod.Namespace, node.Name)
+		return true, nil, nil
+	}
+	log.Printf("pod %v/%v is unlucky to fit on node %v\n", pod.Name, pod.Namespace, node.Name)
+	return false, []string{LuckyPredFailMsg}, nil
+}
+
+// filter filters nodes according to predicates defined in this extender
+// it's webhooked to pkg/scheduler/core/generic_scheduler.go#findNodesThatFit()
+func filter(args schedulerapi.ExtenderArgs) *schedulerapi.ExtenderFilterResult {
+	var filteredNodes []v1.Node
+	failedNodes := make(schedulerapi.FailedNodesMap)
+	pod := args.Pod
+
+	for _, node := range args.Nodes.Items {
+		fits, failReasons, _ := podFitsOnNode(pod, node)
+		if fits {
+			filteredNodes = append(filteredNodes, node)
+		} else {
+			failedNodes[node.Name] = strings.Join(failReasons, ",")
+		}
+	}
+
+	result := schedulerapi.ExtenderFilterResult{
+		Nodes: &v1.NodeList{
+			Items: filteredNodes,
+		},
+		FailedNodes: failedNodes,
+		Error:       "",
+	}
+
+	return &result
+}
+
+// it's webhooked to pkg/scheduler/core/generic_scheduler.go#prioritizeNodes()
+// you can't see existing scores calculated so far by default scheduler
+// instead, scores output by this function will be added back to default scheduler
+func prioritize(args schedulerapi.ExtenderArgs) *schedulerapi.HostPriorityList {
+	pod := args.Pod
+	nodes := args.Nodes.Items
+
+	hostPriorityList := make(schedulerapi.HostPriorityList, len(nodes))
+	for i, node := range nodes {
+		score := rand.Int63n(schedulerapi.MaxExtenderPriority + 1)
+		log.Printf(luckyPrioMsg, pod.Name, pod.Namespace, score)
+		hostPriorityList[i] = schedulerapi.HostPriority{
+			Host:  node.Name,
+			Score: score,
+		}
+	}
+
+	return &hostPriorityList
+}
+
+// Example from https://github.com/Huang-Wei/sample-scheduler-extender
 func main() {
-	route := router.New()
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/filter", Filter)
+	router.GET("/prioritize", Prioritize)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
